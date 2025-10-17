242448719dfafbee7f29c9562aa3c1ab09ce2104
Jennifenayanne <mello.jennifer@escola.pr.gov.br>
2025-10-17 11:29:32 -0300

    upd

---
+++ app.py
@@ -0,0 +1,86 @@
+player ={
+    '1':{
+        'nome':'NILSON',
+        'idade':24,
+        'ryos':320.50,
+        'classe':'Mago',
+        'familia':'Loki',
+        'habilidade':{
+            'ataque':10,
+            'defesa':5,
+            'vida':10,
+        },
+         'nivel':100,
+         'xp':0,
+         'equipamento':[2],
+         'dungeon':1,
+    '2':{
+        'nome':'Jennifer',
+        'idade':18,
+        'ryos':500.50,
+        'classe':'Mago',
+        'familia':'Black',
+        'habilidade':{
+            'ataque':10,
+            'defesa':10,
+            'vida':10,
+        },
+        'nivel':1,
+        'xp':0,
+
+        'equipamento':[2],
+        'dungeon':1,
+    }
+        }
+equipamentos = {
+    '1': {
+        'nome': 'Livro Velho',
+        'classe': 'Mago',
+        'habilidades': {
+            'ataque': 1,
+            'defesa': 1,
+            'vida': 0,}
+    }
+
+   '2': {
+    'nome': 'Espada de madeira',
+    'classe': 'Mago',
+    'habilidades': {
+        'ataque': 1,
+        'defesa': 0,
+        'vida': 1,}
+   },
+
+'3': {
+  'nome': 'Arco de Madeiro',
+  'classe': "Mago",
+  'habilidades':{
+      'ataque': 1,
+      'defesa': 0,
+      'vida':0,}
+  },
+}
+monstros = {
+    '1': {
+        'nome': 'Rato',
+        'habilidade': {
+            'ataque': 1,
+            'defesa': 0,
+            'vida': 1,
+        },
+        '2': {
+            'nome': 'Formiga',
+            'habilidades': {
+                'ataque': 2,
+                'defesa': 2,
+                'vida': 2,
+            }
+        },
+    }'3':{
+           'nome': 'Coelho',
+            'habilidades': {
+                'ataque': 4,
+                'defesa': 3,
+                'vida': 1,
+}
+
